ec3e2cc4a195655658de722bf25c2f750af46aac
Eura Choi <eurachoi@huys-mbp.gateway.sonic.net>
2020-04-08 14:18:47 -0700

    First commit

---
+++ Practice App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Practice App
+//
+//  Created by Eura Choi on 3/27/20.
+//  Copyright © 2020 Eura Choi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
